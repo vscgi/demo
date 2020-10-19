e87ad3b37f5745edf71b7dd5b8b5e7ab2bfffbf5
Schraub <valentin.schraub@cgi.com>
2020-10-19 14:38:54 +0200

    Added Project Lombok

--- src/main/java/com/cgi/springboot/mysqlaccess/Book.java
+++ src/main/java/com/cgi/springboot/mysqlaccess/Book.java
@@ -3,34 +3,15 @@ package com.cgi.springboot.mysqlaccess;
 import javax.persistence.Entity;
 import javax.persistence.Id;
 
+import lombok.Data;
+
+@Data
 @Entity
 public class Book {
 	@Id
 	private int isbn;
+	
 	private String title;
+	
 	private String author;
-	
-	public int getIsbn() {
-		return isbn;
-	}
-	
-	public void setIsbn(int isbn) {
-		this.isbn = isbn;
-	}
-	
-	public String getTitle() {
-		return title;
-	}
-	
-	public void setTitle(String title) {
-		this.title = title;
-	}
-	
-	public String getAuthor() {
-		return author;
-	}
-	
-	public void setAuthor(String author) {
-		this.author = author;
-	}
 }
